6b5dd7afe5fc7af1737937c79fde1d6612a28d49
Ashekur Rahman <ashekur@peymynt.com>
2022-08-28 09:13:45 +0600

    28_august_2022

--- pages/components/Navbar/Navbar.jsx
+++ pages/components/Navbar/Navbar.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import {React, useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 import styles from './Navbar.module.css'
+import Logo from './images/logo-light.svg'
+import { MdMenuOpen,MdClose } from 'react-icons/md';
+
 
 const Navbar = () => {
+  const [isMobileMenu, setIsMobileMenu] = useState(false);
   return (
-    <div className={styles.navbar_area} >
-
-
+    <div className={`${isMobileMenu && styles.mobile_nav_open} ${styles._main_area}`} >
+      <div className="container">
+        <div className={styles._row} >
+          <div className={styles._logo} >
+            <Link href='/home'><Image src={Logo} alt="Vercel Logo" width={193} height={42} /></Link>
+          </div>
+          <div className={styles._menu} >
+            <button onClick={() => setIsMobileMenu(!isMobileMenu)} className={`${styles._toggle} btn btn-none`} ><MdClose /></button>
+            <ul>
+              <li><Link href='/'>Intro</Link></li>
+              <li><Link href='/'>Demos</Link></li>
+              <li><Link href='/'>Documentation</Link></li>
+              <li><Link href='/'>Support</Link></li>
+            </ul>
+          </div>
+          <div className={styles._action} >
+            <Link href='/' ><span className={`btn btn-primary`}>Purchase Now</span></Link>
+            <button onClick={() => setIsMobileMenu(!isMobileMenu)} className={`${styles._toggle} btn btn-none`} ><MdMenuOpen /></button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
