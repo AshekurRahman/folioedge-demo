e017def924e5fcfbdba0a2ccc2fdfa506f8f280b
Ashekur Rahman <ashekur@peymynt.com>
2022-08-31 21:36:54 +0600

    added demo link

--- pages/pages/Home/Demos/Demos.jsx
+++ pages/pages/Home/Demos/Demos.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import SectionTitle from '../../../common/SectionTitle/SectionTitle';
 import styles from './Demo.module.css'
 import image1 from './images/home_1.jpg';
@@ -19,46 +20,52 @@ const Demos = () => {
             </div>
             <div className="row g-5">
                 <div className="col-lg-6">
-                    <div className={styles._demo_item}>
-                        <div className={styles._demo_image} style={{backgroundImage: `url(${image1.src})`}}>
-                        </div>
-                        <div className={styles._demo_title}>Homepage One</div>
-                    </div>
+                    <Link href="http://wp.devignedge.com/folioedge/" >
+                        <div className={styles._demo_item} >
+                            <div className={styles._demo_image} style={{backgroundImage: `url(${image1.src})`}}></div>
+                            <div className={styles._demo_title}>Homepage One</div>
+                        </div>                        
+                    </Link>
+                </div>
+                <div className="col-lg-6">                    
+                    <Link href="http://wp.devignedge.com/folioedge/home-2" >
+                        <div className={`${styles._demo_item} ${styles._right}`}>
+                            <div className={styles._demo_image} style={{backgroundImage: `url(${image2.src})`}}></div>
+                            <div className={styles._demo_title}>Homepage Two</div>
+                        </div>                      
+                    </Link>
+                </div>
+                <div className="col-lg-6">          
+                    <Link href="http://wp.devignedge.com/folioedge/home-3" >
+                        <div className={styles._demo_item}>
+                            <div className={styles._demo_image} style={{backgroundImage: `url(${image3.src})`}}></div>
+                            <div className={styles._demo_title}>Homepage Three</div>
+                        </div>                   
+                    </Link>
+                </div>
+                <div className="col-lg-6"> 
+                    <Link href="http://wp.devignedge.com/folioedge/home-4" >
+                        <div className={`${styles._demo_item} ${styles._right}`}>
+                            <div className={styles._demo_image} style={{backgroundImage: `url(${image4.src})`}}></div>
+                            <div className={styles._demo_title}>Homepage Four</div>
+                        </div>                 
+                    </Link>
                 </div>
                 <div className="col-lg-6">
-                    <div className={`${styles._demo_item} ${styles._right}`}>
-                        <div className={styles._demo_image} style={{backgroundImage: `url(${image2.src})`}}>
-                        </div>
-                        <div className={styles._demo_title}>Homepage Two</div>
-                    </div>
+                    <Link href="http://wp.devignedge.com/folioedge/home-5" >
+                        <div className={styles._demo_item}>
+                            <div className={styles._demo_image} style={{backgroundImage: `url(${image5.src})`}}></div>
+                            <div className={styles._demo_title}>Homepage Five</div>
+                        </div>             
+                    </Link>
                 </div>
                 <div className="col-lg-6">
-                    <div className={styles._demo_item}>
-                        <div className={styles._demo_image} style={{backgroundImage: `url(${image3.src})`}}>
-                        </div>
-                        <div className={styles._demo_title}>Homepage Three</div>
-                    </div>
-                </div>
-                <div className="col-lg-6">
-                    <div className={`${styles._demo_item} ${styles._right}`}>
-                        <div className={styles._demo_image} style={{backgroundImage: `url(${image4.src})`}}>
-                        </div>
-                        <div className={styles._demo_title}>Homepage Four</div>
-                    </div>
-                </div>
-                <div className="col-lg-6">
-                    <div className={styles._demo_item}>
-                        <div className={styles._demo_image} style={{backgroundImage: `url(${image5.src})`}}>
-                        </div>
-                        <div className={styles._demo_title}>Homepage Five</div>
-                    </div>
-                </div>
-                <div className="col-lg-6">
-                    <div className={`${styles._demo_item} ${styles._right}`}>
-                        <div className={styles._demo_image} style={{backgroundImage: `url(${image6.src})`}}>
-                        </div>
-                        <div className={styles._demo_title}>Homepage Six</div>
-                    </div>
+                    <Link href="http://wp.devignedge.com/folioedge/home-6" >
+                        <div className={`${styles._demo_item} ${styles._right}`}>
+                            <div className={styles._demo_image} style={{backgroundImage: `url(${image6.src})`}}></div>
+                            <div className={styles._demo_title}>Homepage Six</div>
+                        </div>           
+                    </Link>
                 </div>
             </div>
         </div>
