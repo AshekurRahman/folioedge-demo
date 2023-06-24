9b108606c624db2c50342e5d721d47b98a4f38bd
Ashekur Rahman <ashekurrahman1@gmail.com>
2023-06-24 15:58:53 +0600

    Update pages

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/bootstrap.css'
-import '../styles/webfonts.css'
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
